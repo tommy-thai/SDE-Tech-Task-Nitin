b0967008ff49fd55a2dbaf93ff02b717e70d86a7
dalvi202 <nitin.dalvi202@gmail.com>
2024-10-15 14:32:30 +0530

    Update schema_setup.sql

--- sql/schema_setup.sql
+++ sql/schema_setup.sql
@@ -1,1 +1,9 @@
 -- SQL queries to set up schemas in BigQuery
+
+CREATE TABLE `data-dev.ntntemp.post` (
+  userId INT64 NOT NULL,
+  id INT64 NOT NULL,
+  title STRING,
+  body STRING,
+  source STRING
+)
